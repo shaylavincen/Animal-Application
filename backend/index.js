77032763e222fcfb14de14a56b3edd56efa32d69
shaylavincen <shayla.vincent@snhu.edu>
2025-01-31 20:28:10 -0500

    connected mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
+const mongoose = require("mongoose");
 const app = express();
 const port = 3000;
 
 app.use(express.json())
 
-app.listen(port, () => {
-    console.log(`backend  listening ${port}`);
-})
+app.get("/", async (req, res) => {
+    return res.json({ message: "Animal Application" });
+});
+
+const start = async () => {
+    try {
+        await mongoose.connect(
+            "mongodb://aacuser:SNHU1234@localhost:27017/aac?authSource=admin"
+        );
+
+        app.listen(3000, () => console.log(`Server started on port ${port}`));
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+start();
